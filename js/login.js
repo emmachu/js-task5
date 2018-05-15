a63cb710a00e6447b27f8fbe8046c6281a8f5307
emmachu <335150350@qq.com>
2018-05-15 19:29:25 +0800

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -5,21 +5,44 @@ $(document).ready(function() {
 	
 	$(".btn").click(function() {
 		var username = user.val();
-		var pw = password.val();
-		// var tip = tips.html();
+		var pwd = password.val();
+		var formdata = new FormData();
 		console.log(username);
-		console.log(pw);
+		console.log(pwd);
+		formdata.append("name", username);
+		formdata.append("pwd", pwd);
+		console.log(formdata);
+		function loadXMLDoc() {
+			var xmlhttp;
+			if (window.XMLHttpRequest)
+			{
+				//  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+				xmlhttp = new XMLHttpRequest();
+			}else {
+				// IE6, IE5 浏览器执行代码
+				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.open("POST","/carrots-admin-ajax/a/login",true);
+			xmlhttp.send(formdata);
+			xmlhttp.onreadystatechange=function() {
+				if (username == " " && pwd == " ") {
+					tips.html("账户密码不能为空。");
+				}else if (username != "admin") {
+					tips.html("无此用户。");
+				}else if (xmlhttp.readyState==4 && xmlhttp.status==200) {
 
-		if (username == false || pw == false) {
-			tips.html("账户密码不能为空。");
-		}else if (username == "admin" && pw == "123456") {
-			sessionStorage.setItem("username", username);
-			sessionStorage.setItem("pw", pw);
-			// window.open("http://www.baidu.com");
-			// window.location.href = "http://www.baidu.com";
-			window.location.href = "http://dev.admin.carrots.ptteng.com/";
-		}else {
-			tips.html("账户或密码错误。");
-		}
-	})
+					var result = JSON.parse(xmlhttp.responseText);
+					console.log(result.message);
+					console.log(typeof result.message);
+					if (result.message === "success") {
+					// window.location.href = "http://dev.admin.carrots.ptteng.com/";
+					}else {
+						tips.html(result.message);
+					}
+			}
+			
+		};
+		loadXMLDoc();
+	});
+
 });
